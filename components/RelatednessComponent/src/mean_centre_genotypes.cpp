f30f4b45e01b57c99a670cb0d22fcdf92eb69b11
Gavin Band <gavin.band@well.ox.ac.uk>
2016-05-13 23:18:14 +0100

    Removed non-bingwa-related stuff

--- components/RelatednessComponent/src/mean_centre_genotypes.cpp
+++
@@ -1,9 +0,0 @@
-
-//          Copyright Gavin Band 2008 - 2012.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
-
-namespace pca {
-	// void void mean_centre_genotypes() is now a template function.
-}
